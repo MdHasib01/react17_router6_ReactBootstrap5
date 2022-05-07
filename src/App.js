329ab6fc8a068f859be5c98db16bf007fd3b4047
Md Hasib <md.hasibuzzaman001@google.com>
2022-05-08 01:41:32 +0600

    react with old version

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <h2>Hello from react v17 and Router v6 </h2>
+    </div>
+  );
+}
+
+export default App;
